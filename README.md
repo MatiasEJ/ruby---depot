bc01fceccb1e1f7b24c75e067e10a80e08a95f06
MatiasEJ <don.juncos@gmail.com>
2021-11-25 19:21:43 -0300

    añade rango de fechas de nacimiento en la creacion de persona

--- README.md
+++ README.md
@@ -22,3 +22,36 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+En este mail se detallan las consignas del challenge y se adjuntan los libros correspondientes para resolverlo:
+
+Seguir el libro de "Agile development with rails 5" donde se muestra paso por paso como crear un proyecto en Rails. 
+
+Según lo explicado en el libro nos gustaría que se cree un nuevo proyecto y que tenga lo siguiente:
+
+
+1- Un Scaffold para el modelo Persona (lista de personas, formulario de edición y pantalla de detalles de persona).
+
+2- El modelo de persona deberá tener los siguientes métodos (parámetros): Nombre, Apellido, Fecha Nacimiento y nro DNI (numerico). Y guardar dichos datos en una base de datos. Se puede usar sqlite.
+
+3- En la pantalla de detalles de cada persona se debe ver la edad en años 
+
+4- En el listado de personas queremos ver el promedio de edad en años del conjunto de personas cargadas al sistema.
+
+5- Queremos la lista de personas ordenadas por MES únicamente. (ej: todos los nacidos en enero y luego los nacidos en febrero,sin importar el año de nacimiento).
+
+ 
+
+* Entregar el proyecto comprimido en un ZIP.
+
+* El código debe estar bien indentado (Nosotros utilizamos 2 espacios para indentar, no con TAB)
+
+EJEMPLO:
+
+  if n > 0
+
+    #esto es con dos espacios
+
+         #esto es con TAB
+
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#---
+# Excerpted from "Agile Web Development with Rails 5",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
+#---
+# encoding: utf-8
+Persona.delete_all
+Persona.create!(
+    nombre: 'Juan',
+    apellido: 'Juanes',
+    fecha_nac: '2016-12-12',
+    dni:12123123
+)
+# . . .
+Persona.create!(
+    nombre: 'Diego',
+    apellido: 'Diegues',
+    fecha_nac: '2016-12-12',
+    dni:12123123
+)
+# . . .
+# Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
+#   description:
+#     %{<p>
+#       <em>Native Apps, Multiple Platforms</em>
+#       Answer the question “Can we build this for ALL the devices?” with a
+#       resounding YES. This book will help you get there with a real-world
+#       introduction to seven platforms, whether you’re new to mobile or an
+#       experienced developer needing to expand your options. Plus, you’ll find
+#       out which cross-platform solution makes the most sense for your needs.
+#       </p>},
+#   image_url: '7apps.jpg',
+#   price: 26.00)
+# . . .
+
