6cf383a5cfc8191fa33e2f2746347f8abe3a233e
MatiasEJ <don.juncos@gmail.com>
2021-12-30 09:59:39 -0300

    elimina un producto del carro

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -53,10 +53,20 @@ class LineItemsController < ApplicationController
 
   # DELETE /line_items/1 or /line_items/1.json
   def destroy
-    @line_item.destroy
+    @line_item = LineItem.find(params[:id])
+    product = Product.find(@line_item.product_id)
+    @cart = Cart.find(@line_item.cart_id)
+    @line_item = @cart.remove_product(product) 
+
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
-      format.json { head :no_content }
+      if @line_item.save
+        format.html { redirect_to store_index_url}
+        format.js   { @current_item = @line_item }
+        format.json { render :show, status: :created, location: @line_item }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
     end
   end
 
