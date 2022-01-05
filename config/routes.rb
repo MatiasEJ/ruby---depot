88eeebb6ac6028d97ca73042eb478528e6c3daf7
matias <don.juncos@gmail.com>
2022-01-04 21:10:23 -0300

    añade atom feeds

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   root 'store#index', as: 'store_index'
   resources :products
   resources :personas
+
+  resources :products do
+    get :who_bought, on: :member
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
